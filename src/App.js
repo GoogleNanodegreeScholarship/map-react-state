a34c1f826f13a41d5ee0c4ec19bc38687563f698
americanman <snowboardracer7@gmail.com>
2018-09-10 14:50:22 -0400

    Example of mapping JSON response

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      array: [1, 2, 3, 4, 5]
     }
   }
 
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users/')
-      .then(response => response.json())
-      .then(data => this.setState({
-        data
-      }))
-  }
-
   render() {
     return (
       <div className="App">
+        <p>{this.state.array.map((number) =>{
+          return <p>number {number} </p>
+        })} </p>
+        <p>{this.state.array.map((number) =>{
+          return <th>comumn {number} </th>
+        })}</p>
+        <h1>{this.state.array.map((number) => {
+          return <td style={{display: 'flex'}}>row {number} </td>
+        })}</h1>
       </div>
     );
   }
